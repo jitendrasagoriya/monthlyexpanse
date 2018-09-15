8108c0227c895e6d710ee12c5e0d3d76f4fb67ce
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-09-15 11:13:03 +0530

    initial commit

---
+++ src/main/java/com/flatmate/expanse/MonthlyexpanseApplication.java
@@ -0,0 +1,12 @@
+package com.flatmate.expanse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyexpanseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyexpanseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flatmate/expanse/MonthlyexpanseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flatmate.expanse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonthlyexpanseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
